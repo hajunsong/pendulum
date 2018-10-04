8cf0bb9f7c7579b7f44fce4b2727ccb93958790e
hajunsong <hajunsong90@gmail.com>
2018-10-04 23:52:05 +0900

    .

--- Pendulum/main.cpp
+++ Pendulum/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 	for (uint i = 0; i <= 1e10*0; i++) {
-		Matrix3 a(1), b;
+		Matrix a(3,3), b(3,3);
 		cout << "a : " << a << endl;
 
 		b(0, 0) = 1;
@@ -14,22 +14,33 @@ int main() {
 		a << 1, 2, 3, 4, 5, 6, 7, 8, 9;
 		cout << "a : " << a << endl;
 
-		Matrix3 c = a*a*a*a*a*a*a;
+		Matrix c = a*a*a*a*a*a*a;
 		cout << "c = a*a*a*a*a*a*a : " << c << endl;
 
-		Matrix3 d = a + c + c + c + c + c + c + c;
+		Matrix d = a + c + c + c + c + c + c + c;
 		cout << "a + c : " << d << endl;
 
-		Matrix3 A;
+		Matrix A(3,3);
 		A << 1, 2, 3, 4, 5, 6, 7, 8, 9;
-		Vector3 B;
+		Vector B(3);
 		B << 1, 3, 4;
-		cout << A * B << endl;
+		cout << "A*B : " << A * B << endl;
 
-		Matrix6 A2;
-		Vector6 B2;
-		cout << A2 * B2 << endl;
+		Matrix A2(6,6);
+		Vector B2(6);
+		cout << "A2*B2 : " << A2 * B2 << endl;
+
+		Matrix C(3, 6);
+		C = (A, A) << (A, A);
+		cout << "C : " << C << endl;
+
+		Matrix aa(3,3);
+		aa << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+		Vector bb(3);
+		bb << 11, 22, 33;
 	}
+
+	Pendulum pen;
 
 	system("pause");
 	return 0;
--- Pendulum/pendulum.h
+++ Pendulum/pendulum.h
@@ -12,20 +12,76 @@ using namespace std;
 
 typedef unsigned int uint;
 
-template <uint row, uint col>
 class Matrix {
 public:
 	uint rows = 0, cols = 0, len = 0;
 	vector<double> data;
 
-	Matrix(double init_value = 0) {
+	Matrix(uint row, uint col=1) {
 		rows = row;
 		cols = col;
 		len = rows * cols;
-		data.assign(len, init_value);
+		data.assign(len, 0);
 	}
+	Matrix() {}
 	~Matrix() {
 		data.clear();
+	}
+	Matrix t() {
+		Matrix mat(rows, cols);
+		for (uint i = 0; i < rows; i++) {
+			for (uint j = 0; j < cols; j++) {
+				mat(j, i) = data[i*cols + j];
+			}
+		}
+		return mat;
+	}
+	static Matrix zeros(uint row, uint col=1) {
+		Matrix mat(row, col);
+		for (uint i = 0; i < mat.rows; i++) {
+			for (uint j = 0; j < mat.cols; j++) {
+				mat(j, i) = 0;
+			}
+		}
+		return mat;
+	}
+	static Matrix eye(uint m_size) {
+		Matrix mat(m_size, m_size);
+		for (uint i = 0; i < m_size; i++) {
+			for (uint j = 0; j < m_size; j++) {
+				mat(j, i) = i == j ? 1 : 0;
+			}
+		}
+		return mat;
+	}
+	Matrix col(uint col_indx) {
+		Matrix mat(rows, 1);
+		for (uint i = 0; i < rows; i++) {
+			mat(i, col_indx) = data[i*cols + col_indx];
+		}
+		return mat;
+	}
+	Matrix row(uint row_indx) {
+		Matrix mat(1, cols);
+		for (uint i = 0; i < cols; i++) {
+			mat(row_indx, i) = data[row_indx*cols + i];
+		}
+	}
+	Matrix insertCol(Matrix mat_in, uint start_indx, uint end_indx) {
+		for (uint i = 0; i < rows; i++) {
+			for (uint j = start_indx; j <= end_indx; j++) {
+				this->operator()(i, j) = mat_in(i, j);
+			}
+		}
+		return *this;
+	}
+	Matrix insertRow(Matrix mat_in, uint start_indx, uint end_indx) {
+		for (uint i = start_indx; i <= end_indx; i++) {
+			for (uint j = 0; j <= cols; j++) {
+				this->operator()(i, j) = mat_in(i, j);
+			}
+		}
+		return *this;
 	}
 	double& operator()(uint row_indx, uint col_indx=0) {
 		return data[row_indx*cols + col_indx];
@@ -42,19 +98,19 @@ public:
 		}
 		return os;
 	}
-	template <typename T> Matrix& operator<<(T rhs) {
+	Matrix& operator<<(double rhs) {
 		if (data.size() == len) {
 			data.clear();
 		}
 		data.push_back(rhs);
 		return *this;
 	}
-	template <typename T> Matrix& operator,(T rhs) {
+	Matrix& operator,(double rhs) {
 		data.push_back(rhs);
 		return *this;
 	}
 	Matrix operator*(Matrix rhs) {
-		Matrix<row, col> mat;
+		Matrix mat(rows, rhs.cols);
 		for (uint i = 0; i < rows; i++) {
 			for (uint j = 0; j < rhs.cols; j++) {
 				double temp = 0;
@@ -67,84 +123,66 @@ public:
 		return mat;
 	}
 	template <typename T> Matrix operator*(T value) {
-		Matrix<row, col> mat;
+		Matrix mat(rows, cols);
 		for (uint i = 0; i < len; i++) {
-			mat.data[i] += value;
+			mat.data[i] = data[i] * value;
 		}
 		return mat;
 	}
 	template <typename T> friend Matrix operator*(T value, Matrix mat) {
 		return mat * value;
 	}
+	template <typename T> Matrix operator/(T value) {
+		Matrix mat(rows, cols);
+		for (uint i = 0; i < len; i++) {
+			mat.data[i] = data[i] / value;
+		}
+		return mat;
+	}
+	template <typename T> friend Matrix operator/(T value, Matrix mat) {
+		return mat / value;
+	}
 	Matrix operator+(Matrix rhs) {
-		Matrix<row, col> mat;
+		Matrix mat(rows, cols);
 		for (uint i = 0; i < mat.len; i++) {
 			mat.data[i] = data[i] + rhs.data[i];
 		}
 		return mat;
 	}
 	Matrix operator-(Matrix rhs) {
-		Matrix<row, col> mat;
+		Matrix mat(rows, cols);
 		for (uint i = 0; i < mat.len; i++) {
 			mat.data[i] = data[i] - rhs.data[i];
 		}
 		return mat;
 	}
-	Matrix& operator=(Matrix& rhs) {
+	template <typename T> Matrix operator=(T rhs) {
 		this->rows = rhs.rows;
 		this->cols = rhs.cols;
 		this->len = rhs.len;
 		this->data = rhs.data;
+		return *this;
 	}
-	//Matrix operator<<(Matrix rhs) {
-	//	Matrix<row, col> mat;
-
-	//	return mat;
-	//}
-	//Matrix operator,(Matrix rhs) {
-
-	//}
+	Matrix operator<<(Matrix rhs) {
+		Matrix mat(rows+rhs.rows, rhs.cols);
+		for (uint i = 0; i < mat.rows; i++) {
+			for (uint j = 0; j < mat.cols; j++) {
+				mat(i, j) = i < rows ? data[i*cols + j] : rhs(i - rows, j);
+			}
+		}
+		return mat;
+	}
+	Matrix operator,(Matrix rhs) {
+		Matrix mat(rhs.rows, cols+rhs.cols);
+		for (uint i = 0; i < mat.rows; i++) {
+			for (uint j = 0; j < mat.cols; j++) {
+				mat(i, j) = j < cols ? data[i*cols + j] : rhs(i, j - cols);
+			}
+		}
+		return mat;
+	}
 };
-
-typedef Matrix<3, 3> Matrix3;
-typedef Matrix<6, 6> Matrix6;
-typedef Matrix<3, 1> Vector3;
-typedef Matrix<6, 1> Vector6;
-
-inline Vector3 operator*(Matrix3 mat, Vector3 vec) {
-	Vector3 vec_result;
-	for (uint i = 0; i < mat.rows; i++) {
-		double temp = 0;
-		for (uint j = 0; j < mat.cols; j++) {
-			temp += mat(i, j)*vec(j);
-		}
-		vec_result(i) = temp;
-	}
-	return vec_result;
-}
-inline Vector6 operator*(Matrix6 mat, Vector6 vec) {
-	Vector6 vec_result;
-	for (uint i = 0; i < mat.rows; i++) {
-		double temp = 0;
-		for (uint j = 0; j < mat.cols; j++) {
-			temp += mat(i, j)*vec(j);
-		}
-		vec_result(i) = temp;
-	}
-	return vec_result;
-}
-
-//template <typename T, typename U> T operator*(U mat, T vec) {
-//	T vec_result;
-//	for (uint i = 0; i < mat.rows; i++) {
-//		double temp = 0;
-//		for (uint j = 0; j < mat.cols; j++) {
-//			temp += mat(i, j)*vec(j);
-//		}
-//		vec_result(i) = temp;
-//	}
-//	return vec_result;
-//}
+typedef Matrix Vector;
 
 class Pendulum
 {
